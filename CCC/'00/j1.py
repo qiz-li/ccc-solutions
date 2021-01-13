fac2c4f1b370f31307bdf0f5d03110f739daae2c
qiz-li <47799695+qiz-li@users.noreply.github.com>
2021-01-12 20:14:38 -0800

    🎉  feat: Initial commit
    Add solutions to the Canadian Computing Contest,
    first, add 2000 J1 - Calendar.
    
    This repo was grouped with other solutions into one big repo,
    but now I have decided to separate them.

---
+++ CCC/'00/j1.py
@@ -0,0 +1,16 @@
+# CCC '00 J1 - Calendar
+# Find this problem at:
+# https://dmoj.ca/problem/ccc00j1
+
+problem_input = [int(i) for i in input().split()]
+# Start by padding until the day of the week on which the month begins
+calender = ('   ,' * (problem_input[0]-1)).split(',')[:-1]
+# Add the approviate whitespace into each day
+for i in range(1, problem_input[1]+1):
+    calender.append(f'{str(i): >3}')
+# Add the heading and split the calendar into weeks
+full_calender = ([["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"]] +
+                 [calender[i: i + 7] for i in range(0, len(calender), 7)])
+# Print out the calendar
+for week in full_calender:
+    print(' '.join(week))
