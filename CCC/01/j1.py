a8b05a05dac32830d83e928d1b72f773e20551ae
qiz-li <47799695+qiz-li@users.noreply.github.com>
2021-01-15 20:24:40 -0800

    ✨  feat: Add 2001 J1 - Dressing Up

---
+++ CCC/01/j1.py
@@ -0,0 +1,10 @@
+# CCC '01 J1 - Dressing Up
+# Find this problem at:
+# https://dmoj.ca/problem/ccc01j1
+
+height = int(input())
+# These are the two parts of the bow tie
+for half in (range(1, height, 2), range(height, -1, -2)):
+    # Print each line accordingly
+    for i in half:
+        print('*' * i + ' ' * ((height-i)*2) + '*' * i)
